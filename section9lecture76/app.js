09035af3aeb01440b193325605d01271ecf9a51c
Vít Herain <vit.herain@gmail.com>
2017-04-23 21:24:35 +0200

    Section 9, Lecture 76

---
+++ section9lecture76/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/person/:id', function(req, res) {
+    res.render('person', {ID: req.params.id});
+});
+
+app.get('/api', function(req, res) {
+    res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
+app.listen(port);
