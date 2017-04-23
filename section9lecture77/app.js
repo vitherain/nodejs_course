295507a41d2377c55d0a2c95b6467784b0a89238
Vít Herain <vit.herain@gmail.com>
2017-04-23 21:54:18 +0200

    Section 9, LEcture 77

---
+++ section9lecture77/app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/person/:id', function(req, res) {
+    res.render('person', {
+        ID: req.params.id, 
+        Qstr: req.query.qstr 
+    });
+});
+
+app.post('/person', urlencodedParser, function(req, res) {
+    res.send('Thank you!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function(req, res) {
+    res.send('Thank you for the JSON data!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+app.get('/api', function(req, res) {
+    res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
+app.listen(port);
